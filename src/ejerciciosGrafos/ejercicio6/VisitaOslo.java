ca99cae0c0a55eab628e342c86fee94adf86047e
FrancoGarcia <francogarcia1331@gmail.com>
2022-06-12 19:46:57 -0300

    ejercicio 6 hecho

--- src/ejerciciosGrafos/ejercicio6/VisitaOslo.java
+++ src/ejerciciosGrafos/ejercicio6/VisitaOslo.java
@@ -11,40 +11,72 @@ public class VisitaOslo {
     }
 
 
-    public void buscarVerticeInicial(String ciudad1,Grafo<String> grafo, String destino, int maxTiempo, ListaGenerica<String> restringidos){
+    public int buscarVerticeInicial(String lugarInicial,Grafo<String> grafo,boolean[] marca,ListaGenerica<String> restringidos){
         ListaEnlazadaGenerica<String> lis = new ListaEnlazadaGenerica<String>();
-        boolean[] marca = new boolean[grafo.listaDeVertices().tamanio()+1];
         for(int i = 1; i < marca.length; i++){
             marca[i] = false;
         }
 
-        ListaGenerica<Vertice<String>> vInicial = mapaCiudades.listaDeVertices();
-        vInicial.comenzar(); //se busca el vertice en el cual inicia el recorrido
+        ListaGenerica<Vertice<String>> vertices = mapaCiudades.listaDeVertices();
+        vertices.comenzar(); //se busca el vertice en el cual inicia el recorrido
 
-        Vertice<String> v;
+        Vertice<String> vInicio;
         boolean encontrado = false;
         int pos = -1;
 
-        while(!vInicial.fin()){
-            v = vInicial.proximo();
-            if(v.dato() == ciudad1){
+        while((!vertices.fin())&&(!encontrado)){
+            vInicio = vertices.proximo();
+            if((vInicio.dato() == lugarInicial)&&(!restringidos.incluye(lugarInicial))){
                 encontrado = true;
-                pos = v.getPosicion();
-                lis.agregarFinal(v.dato());
+                pos = vInicio.getPosicion();
+                lis.agregarFinal(vInicio.dato());
             }
         }
         if(pos != -1){
             marca[pos] = true;
         }
+        return pos;
     }
 
-    public ListaGenerica<String> paseoEnBici(Grafo<String> grafo,String destino,int maxTiempo,ListaGenerica<String> lugaresRestringidos){
-        ListaEnlazadaGenerica<String> camino = new ListaEnlazadaGenerica<String>();
-        ListaEnlazadaGenerica<String> lista = new ListaEnlazadaGenerica<String>();
-        String ciudad1 = "gol"; //por poner algo
+    public ListaEnlazadaGenerica<Vertice<String>> paseoEnBici(Grafo<String> grafo,String destino,int maxTiempo,ListaGenerica<String> lugaresRestringidos){
+        ListaEnlazadaGenerica<Vertice<String>> camino = new ListaEnlazadaGenerica<Vertice<String>>();
+        ListaEnlazadaGenerica<Vertice<String>> lista = new ListaEnlazadaGenerica<Vertice<String>>();
+        boolean[] marca = new boolean[grafo.listaDeVertices().tamanio()+1];
+        String lugarInicial = "Ingresar un nombre"; //por poner algo
 
-        buscarVerticeInicial(ciudad1, grafo, destino, maxTiempo, lugaresRestringidos);
+        int tiempoMax = 120;
+        int posVerticeInicial = buscarVerticeInicial(lugarInicial,grafo,marca,lugaresRestringidos); //hay que controlar que el vertice inicial no
+        //sea un lugar restringido?
+        int costoTiempo = 0;
+        buscarCamino(posVerticeInicial,grafo,lista,tiempoMax,lugaresRestringidos,costoTiempo,camino,marca,destino);
 
         return camino;
     }
+
+    public void buscarCamino(int posIni,Grafo<String> grafo,ListaEnlazadaGenerica<Vertice<String>> lista,
+                             int tMax,ListaGenerica<String> restringidos, int costoTiempo,
+                             ListaEnlazadaGenerica<Vertice<String>> camino, boolean[] marca,String destino){
+        int j = 0;
+        Vertice<String> inicio = grafo.listaDeVertices().elemento(posIni);
+        ListaGenerica<Arista<String>> ady = grafo.listaDeAdyacentes(inicio);
+        Vertice<String> vertice = null;
+        ady.comenzar();
+        while((!ady.fin())&&(!vertice.dato().equals(destino))){
+            Arista<String> arista = ady.proximo();
+            j = arista.verticeDestino().getPosicion();
+            if(!marca[j]){
+                costoTiempo += arista.peso();
+                vertice = arista.verticeDestino();
+                lista.agregarFinal(vertice);
+                marca[j] = true;
+                if((costoTiempo <= tMax)&&(!restringidos.incluye(vertice.dato()))){
+                    camino.clonar(lista,camino);
+                }
+                lista.eliminarEn(j);
+                marca[j] = false;
+            }
+            buscarCamino(j,grafo,lista,tMax,restringidos,costoTiempo,camino,marca,destino);
+        }
+    }
+
 }
--- src/ejerciciosTeoria/costoTotalCamino/RecorridosTeoria.java
+++ src/ejerciciosTeoria/costoTotalCamino/RecorridosTeoria.java
@@ -1,4 +1,51 @@
 package ejerciciosTeoria.costoTotalCamino;
 
+import tp02.ejercicio2.*;
+import tp06.ejercicio3.*;
+
 public class RecorridosTeoria {
+    public ListaGenerica<ListaGenerica<Vertice>> dfsConCosto(Grafo<T> grafo){
+        boolean[] marca = new boolean[grafo.listaDeVertices().tamanio()+1];
+        ListaGenerica<Vertice<T>> lista = null;
+        ListaGenerica<ListaGenerica<Vertice<T>>> recorridos =
+                    new ListaGenerica<ListaGenerica<Vertice<T>>>();
+
+        int costo = 0;
+        for(int i = 1; i < marca.length; i++){
+            lista = new ListaGenericaEnlazada<Vertice<T>>();
+            lista.agregarFinal(grafo.listaDeVertices().elemento(i));
+            marca[i] = true;
+            dfsConCostoRecursivo(i,grafo,lista,marca,costo,recorridos);
+            marca[i] = false;
+        }
+
+        return recorridos;
+    }
+
+    private void dfsConCostoRecursivo(int pos,Grafo<T> grafo,ListaGenerica<T>lista,boolean[] marca,int costo,ListaGenerica<Vertice<T>>recorridos){
+        Vertice<T> vDestino = null; int p = 0, j = 0;
+        Vertice<T> v = grafo.listaDeVertices().elemento(pos);
+        ListaGenerica<Arista<T>> ady = grafo.listaDeAdyacentes(v);
+        ady.comenzar();
+        while(!ady.fin()){
+            Arista<T> arista = ady.proximo();
+            j = arista.verticeDestino().getPosicion();
+            if(!marca[j]){
+                p = arista.peso();
+                if((costo + p) < 10 ){
+                    vDestino = arista.verticeDestino();
+                    lista.agregarFinal(vDestino);
+                    marca[j] = true;
+                    if((costo + p) == 10){
+                        recorridos.agregarFinal(lista.clonar());
+                    }else{
+                        dfsConCostoRecursivo(j,grafo,lista,marca,costo+p,recorridos);
+                    }
+                    lista.eliminarEn(j);
+                    marca[j] = false;
+                }
+            }
+        }
+    }
+
 }
