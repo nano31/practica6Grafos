b9b2f0d4254a349fb677d387d90ee05d13b812c9
Franco Garcia Carril <78042300+nano31@users.noreply.github.com>
2022-07-01 15:49:26 -0300

    hacer ejercicio mapaAristasCero

---
+++ src/ejerciciosGrafos/ejercicio6/mapaAristasCero.java
@@ -0,0 +1,3 @@
+/*se tiene un grafo llamado mapa que es variable de instancia de la clase Recorridos.
+Se debe ir desde un origen a un destino solamente pasando por aristas que no tengan peso, es decir,
+que su peso sea cero.*/
